5f4e8a11e19a612455e6a68ecf78cc9b32779f95
Thibaut Assus <thibaut.assus@gmail.com>
2014-10-15 15:22:59 +0200

    First version of selected

---
+++ lib/selected.rb
@@ -0,0 +1,24 @@
+class Selected
+  def selected?(controller_name, action_name="", id="")
+    unless action_name.present?
+      action_name = params[:action]
+    end
+    unless id.present?
+      id = params[:id]
+    end
+
+    if controller_matches(controller_name) && action_matches(action_name) && params[:id] == id
+      "selected"
+    end
+  end
+
+  private
+
+  def controller_matches(controller_name)
+    params[:controller] == controller_name.to_s
+  end
+
+  def action_matches(action_name)
+    (action_name.respond_to?(:each) && action_name.map(&:to_s).include?(params[:action])) || params[:action] == action_name.to_s
+  end
+end
---
+++ selected.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'selected'
+  s.version     = '0.0.1'
+  s.date        = '2014-10-15'
+  s.summary     = "Selected"
+  s.description = "A simple gem to know if a link should get the selected class"
+  s.authors     = ['Thibaut Assus', 'Melissa Grach']
+  s.email       = 'thibaut@milesrock.com'
+  s.files       = ["lib/selected.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/selected'
+  s.license       = 'MIT'
+end
